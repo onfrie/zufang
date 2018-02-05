0b817b33c8ff1ce165d7e925d6c2bea9982c1e31
zhouyaxu1 <281979199@qq.com>
2018-02-05 22:36:17 +0800

    提交布局

--- app/src/main/java/cn/com/example/haitu/MainActivity.java
+++ app/src/main/java/cn/com/example/haitu/MainActivity.java
@@ -1,7 +1,6 @@
 package cn.com.example.haitu;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
@@ -11,6 +10,8 @@ import android.widget.TextView;
 import com.zyw.horrarndoo.sdk.base.activity.BaseCompatActivity;
 import com.zyw.horrarndoo.sdk.utils.ToastUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.com.example.haitu.ui.fragment.FragmentHome;
 import cn.com.example.haitu.ui.fragment.FragmentHot;
 import cn.com.example.haitu.ui.fragment.FragmentMe;
@@ -19,6 +20,10 @@ import cn.com.example.haitu.ui.fragment.FragmentTalk;
 public class MainActivity extends BaseCompatActivity {
 
 
+    @BindView(R.id.iv_menu_home)
+    ImageView mIvMenuHome;
+    @BindView(R.id.iv_menu_center)
+    ImageView mIvMenuCenter;
     private FragmentManager fManager;
 
     private FragmentHome fragment_home;
@@ -35,13 +40,13 @@ public class MainActivity extends BaseCompatActivity {
     private TextView tv_menu_me;
     private ImageView mIv_center;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initViews();
-        clickMenu(findViewById(R.id.ll_menu_home));
-    }
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        initViews();
+//        clickMenu(findViewById(R.id.ll_menu_home));
+//    }
 
     @Override
     protected void initView(Bundle savedInstanceState) {
@@ -58,19 +63,19 @@ public class MainActivity extends BaseCompatActivity {
     private void initViews() {
         fManager = getSupportFragmentManager();
 
-        iv_menu_home = (ImageView)findViewById(R.id.iv_menu_home);
-        tv_menu_home = (TextView)findViewById(R.id.tv_menu_home);
+        iv_menu_home = (ImageView) findViewById(R.id.iv_menu_home);
+        tv_menu_home = (TextView) findViewById(R.id.tv_menu_home);
 
-        iv_menu_hot = (ImageView)findViewById(R.id.iv_menu_hot);
-        tv_menu_hot = (TextView)findViewById(R.id.tv_menu_hot);
+        iv_menu_hot = (ImageView) findViewById(R.id.iv_menu_hot);
+        tv_menu_hot = (TextView) findViewById(R.id.tv_menu_hot);
 
-        iv_menu_talk = (ImageView)findViewById(R.id.iv_menu_talk);
-        tv_menu_talk = (TextView)findViewById(R.id.tv_menu_talk);
+        iv_menu_talk = (ImageView) findViewById(R.id.iv_menu_talk);
+        tv_menu_talk = (TextView) findViewById(R.id.tv_menu_talk);
 
-        iv_menu_me = (ImageView)findViewById(R.id.iv_menu_me);
-        tv_menu_me = (TextView)findViewById(R.id.tv_menu_me);
+        iv_menu_me = (ImageView) findViewById(R.id.iv_menu_me);
+        tv_menu_me = (TextView) findViewById(R.id.tv_menu_me);
         mIv_center = (ImageView) findViewById(R.id.iv_menu_center);
-        mIv_center.setOnClickListener(new View.OnClickListener() {
+        mIvMenuCenter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ToastUtils.showToast("哈哈");
@@ -79,10 +84,9 @@ public class MainActivity extends BaseCompatActivity {
     }
 
     /**
-     *
      * @param v
      */
-    public void clickMenu(View v){
+    public void clickMenu(View v) {
         FragmentTransaction trans = fManager.beginTransaction();
         int vID = v.getId();
 
@@ -90,31 +94,29 @@ public class MainActivity extends BaseCompatActivity {
 
         hideFrament(trans);
 
-        setFragment(vID,trans);
+        setFragment(vID, trans);
         trans.commit();
     }
 
     /**
-     *
      * @param trans
      */
     private void hideFrament(FragmentTransaction trans) {
-        if(fragment_home!=null){
+        if (fragment_home != null) {
             trans.hide(fragment_home);
         }
-        if(fragment_hot!=null){
+        if (fragment_hot != null) {
             trans.hide(fragment_hot);
         }
-        if(fragment_talk!=null){
+        if (fragment_talk != null) {
             trans.hide(fragment_talk);
         }
-        if(fragment_me!=null){
+        if (fragment_me != null) {
             trans.hide(fragment_me);
         }
     }
 
     /**
-     *
      * @param vID
      * @param
      */
@@ -124,34 +126,34 @@ public class MainActivity extends BaseCompatActivity {
 //            ToastUtils.showToast("哈哈");
 //        }
 
-        if(vID==R.id.ll_menu_home){
+        if (vID == R.id.ll_menu_home) {
             iv_menu_home.setImageDrawable(getResources().getDrawable(R.drawable.home_blue_icn));
             tv_menu_home.setTextColor(getResources().getColor(R.color.colorPrimary));
-        }else {
+        } else {
             iv_menu_home.setImageDrawable(getResources().getDrawable(R.drawable.home_white_icn));
             tv_menu_home.setTextColor(getResources().getColor(R.color.ff444444));
         }
 
-        if(vID==R.id.ll_menu_hot){
+        if (vID == R.id.ll_menu_hot) {
             iv_menu_hot.setImageDrawable(getResources().getDrawable(R.drawable.house_blue_icn));
             tv_menu_hot.setTextColor(getResources().getColor(R.color.colorPrimary));
-        }else {
+        } else {
             iv_menu_hot.setImageDrawable(getResources().getDrawable(R.drawable.house_white_icn));
             tv_menu_hot.setTextColor(getResources().getColor(R.color.ff444444));
         }
 
-        if(vID==R.id.ll_menu_talk){
+        if (vID == R.id.ll_menu_talk) {
             iv_menu_talk.setImageDrawable(getResources().getDrawable(R.drawable.rental_blue_icn));
             tv_menu_talk.setTextColor(getResources().getColor(R.color.colorPrimary));
-        }else {
+        } else {
             iv_menu_talk.setImageDrawable(getResources().getDrawable(R.drawable.rental_white_icn));
             tv_menu_talk.setTextColor(getResources().getColor(R.color.ff444444));
         }
 
-        if(vID==R.id.ll_menu_me){
+        if (vID == R.id.ll_menu_me) {
             iv_menu_me.setImageDrawable(getResources().getDrawable(R.drawable.my_blue_icn_));
             tv_menu_me.setTextColor(getResources().getColor(R.color.colorPrimary));
-        }else {
+        } else {
             iv_menu_me.setImageDrawable(getResources().getDrawable(R.drawable.my_white_icn));
             tv_menu_me.setTextColor(getResources().getColor(R.color.ff444444));
         }
@@ -159,45 +161,53 @@ public class MainActivity extends BaseCompatActivity {
 
     /**
      * ����Fragment
+     *
      * @param vID
      * @param trans
      */
-    private void setFragment(int vID,FragmentTransaction trans) {
+    private void setFragment(int vID, FragmentTransaction trans) {
         switch (vID) {
             case R.id.ll_menu_home:
-                if(fragment_home==null){
+                if (fragment_home == null) {
                     fragment_home = new FragmentHome();
                     trans.add(R.id.content, fragment_home);
-                }else{
+                } else {
                     trans.show(fragment_home);
                 }
                 break;
             case R.id.ll_menu_hot:
-                if(fragment_hot==null){
+                if (fragment_hot == null) {
                     fragment_hot = new FragmentHot();
                     trans.add(R.id.content, fragment_hot);
-                }else{
+                } else {
                     trans.show(fragment_hot);
                 }
                 break;
             case R.id.ll_menu_talk:
-                if(fragment_talk==null){
+                if (fragment_talk == null) {
                     fragment_talk = new FragmentTalk();
                     trans.add(R.id.content, fragment_talk);
-                }else{
+                } else {
                     trans.show(fragment_talk);
                 }
                 break;
             case R.id.ll_menu_me:
-                if(fragment_me==null){
+                if (fragment_me == null) {
                     fragment_me = new FragmentMe();
                     trans.add(R.id.content, fragment_me);
-                }else{
+                } else {
                     trans.show(fragment_me);
                 }
                 break;
             default:
                 break;
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
